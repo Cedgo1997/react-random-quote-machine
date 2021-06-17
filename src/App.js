824380cc1f406d11d7d34fb653515d4262661c12
Cedgo <cedgo1997@gmail.com>
2021-06-16 22:41:37 -0400

    Changing App.js to FCC project beginning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/heading-has-content */
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quotes: [
+                {
+                    text: 'Live as if you were to die tomorrow. Learn as if you were to live forever.',
+                    author: 'Mahatma Gandhi'
+                },
+                {
+                    text: 'That which does not kill us makes us stronger.',
+                    author: 'Friedrich Nietzsche'
+                },
+                {
+                    text: 'Be who you are and say what you feel, because those who mind don’t matter and those who matter don’t mind.',
+                    author: 'Bernard M. Baruch'
+                },
+                {
+                    text: 'We must not allow other people’s limited perceptions to define us.',
+                    author: 'Virginia Satir'
+                },
+                {
+                    text: 'Do what you can, with what you have, where you are.',
+                    author: 'Theodore Roosevelt'
+                }
+            ],
+            quote: {
+                text: '',
+                author: ''
+            }
+        }
+
+        this.changeQuote = this.changeQuote.bind(this);
+    }
+
+    changeQuote = () => {
+        let index =  Math.floor(Math.random()*this.state.quotes.length);
+        this.setState({
+            quote: {
+                text: this.state.quotes[index].text,
+                author: this.state.quotes[index].author
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div id="quote-box">
+                <h1 id="text">{this.state.quote.text}</h1>
+                <p id="author">{this.state.quote.author}</p>
+                <button onClick={this.changeQuote} id="new-quote">New</button>
+            </div>
+        )
+    }
 }
 
 export default App;
